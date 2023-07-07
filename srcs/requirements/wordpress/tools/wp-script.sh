9c4e6c456de4654f4a923aa6819c1ecd80da4b6f
nali <nali@inception>
2023-07-07 15:29:28 +0400

    added bullseye

--- srcs/requirements/wordpress/tools/wp-script.sh
+++ srcs/requirements/wordpress/tools/wp-script.sh
@@ -20,13 +20,13 @@ sed -i -r "s/db_name_string/${DB_NAME}/1"   wp-config.php
 sed -i -r "s/db_user_string/${AD_USER}/1"  wp-config.php
 sed -i -r "s/db_pwd_string/${AD_PASS}/1"    wp-config.php
 
-wp core install --path=/var/www/html/ --url=localhost --title="Inception by Nousheen" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+wp core install --path=/var/www/html/ --url=nali.42.fr --title="Inception by Nousheen" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 
 wp user create --path=/var/www/html/ $WP_USER $WP_EMAIL --role=contributor --user_pass=$WP_PWD --allow-root
 
 chown -R www-data:www-data /var/www/html/
 
-sed -i 's/listen = .*/listen = wordpress:9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+sed -i 's/listen = .*/listen = wordpress:9000/g' /etc/php/7.4/fpm/pool.d/www.conf
 
 rm -rf /wordpress
 
